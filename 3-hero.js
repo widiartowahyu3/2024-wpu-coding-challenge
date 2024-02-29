9633184b071648dc80f5984d6dea08c42f470a8f
widiartowahyu3 <widiartowahyu3@gmail.com>
2024-02-29 10:00:51 +0700

    day-3/366

--- 3-hero.js
+++ 3-hero.js
@@ -1,5 +1,15 @@
-function hero(bullets, dragons) {
-  //Get Coding!
-}
+// function hero(bullets, dragons) {
+//   if (bullets / 2 >= dragons) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
+// function hero(bullets, dragons) {
+//   //truthy / falsey
+//   return bullets / 2 >= dragons;
+// }
+
+const hero = (bullets, dragons) => bullets / 2 >= dragons;
 console.log(hero(10, 5));
