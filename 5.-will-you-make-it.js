934fe3936abc9a34371d7ab6524ba08501e2e97c
widiartowahyu3 <widiartowahyu3@gmail.com>
2024-02-29 10:44:46 +0700

    day 5 / 366

--- 5.-will-you-make-it.js
+++ 5.-will-you-make-it.js
@@ -1,9 +1,11 @@
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-  if (mpg * fuelLeft >= distanceToPump) {
-    return true;
-  } else {
-    return false;
-  }
-};
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   if (mpg * fuelLeft >= distanceToPump) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
-console.log(zeroFuel(50, 25, 2));
+const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
+  distanceToPump / mpg <= fuelLeft;
+console.log(zeroFuel(50, 25, 10));
